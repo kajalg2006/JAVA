8dbba784869b1433a8f54a5842f64093423ec4b8
Kajal gupta <164746722+kajalg2006@users.noreply.github.com>
2025-04-09 10:39:02 +0530

    Add files via upload

---
+++ Basic.java
@@ -0,0 +1,18 @@
+class   Basic{
+    public static void main(String[] args) {
+        System.out.println("hello "+10+10);
+        System.out.println(10+10+"ji");
+        int a = 6;
+         int b = 78;
+
+         System.out.println(a&b);
+         System.out.println(5<6);
+         boolean x = false;
+         if(x== true){
+            System.out.println("hello");
+         }
+         else{
+            System.out.println("hii");
+         }
+    }
+}
